9adc70a2f3d32a19eae435f89b73f54051d7c1f0
LukaszSzumiec <lukasz.d.szumiec@gmail.com>
2024-12-01 23:20:16 +0100

    Working on a parser for resp request

--- redis/resp/resp_types.py
+++ redis/resp/resp_types.py
@@ -7,14 +7,23 @@ class Errors: ...
 class WholeNumbers: ...
 
 
-class BulkStrings: ...
+class BulkStrings:
+    def __init__(self, bytes_list):
+        self.bytes_list = bytes_list
 
 
-class Tables: ...
+class Tables:
+    def __init__(self, bytes_list):
+        self.bytes_list = bytes_list
+        self.bulk_strings = BulkStrings(self.bytes_list)
 
 
 class Command:
-    def __init__(self):
+    def __init__(self, bytes_list):
+        self.bytes_list = bytes_list
+        self.tables = Tables(self.bytes_list)
+
+    def build(self):
         ...
 
     def parse(self):
--- redis/resp/validator.py
+++ redis/resp/validator.py
@@ -1,3 +1,6 @@
+from redis.resp.resp_types import Command
+
+
 class ValidateRequest:
     def __init__(self, data: bytes):
         print(f"INPUT COMMAND: {data}")
@@ -5,14 +8,32 @@ class ValidateRequest:
         self.bytes_list = [x for x in data]
 
     def validate(self):
-        if self.is_a_list_of_commands():
-            print("1")
+        self._validate()
+        command = Command(self.bytes_list)
+
+        # amount_of_bulk_strings = self._find_amount_of_bulked_strings()
+        # if self.is_table():
+        #     print("1")
         # elif self.
-        else:
-            print("2")
+        # else:
+        #     print("2")
+
+    def _validate(self):
+        for char in self.bytes_list:
+
+
+
+    def _find_amount_of_bulked_strings(self):
+        control_star = self.input_data[0]
+        elements_amount = self.input_data[1]
+        print("CONTROL STAR: ", control_star)
+        print(ord("*"))
+        if control_star != ord("*"):
+            print("Nie zaczyna sie od gwiazdki.")
+            raise Exception
+
+        return elements_amount
+
 
     def is_a_list_of_commands(self):
-        if len(self.bytes_list) and self.bytes_list[0] == 42:
-            return True
-        else:
-            return False
+        ...
