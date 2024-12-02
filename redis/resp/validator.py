a69cf9a16df5f405f9108c7e9cba5086a8f644c3
LukaszSzumiec <lukasz.d.szumiec@gmail.com>
2024-12-02 23:02:57 +0100

    Keep working on resp

--- redis/resp/validator.py
+++ redis/resp/validator.py
@@ -8,20 +8,14 @@ class ValidateRequest:
         self.bytes_list = [x for x in data]
 
     def validate(self):
-        self._validate()
         command = Command(self.bytes_list)
-
+        command.parse()
         # amount_of_bulk_strings = self._find_amount_of_bulked_strings()
         # if self.is_table():
         #     print("1")
         # elif self.
         # else:
         #     print("2")
-
-    def _validate(self):
-        for char in self.bytes_list:
-
-
 
     def _find_amount_of_bulked_strings(self):
         control_star = self.input_data[0]
